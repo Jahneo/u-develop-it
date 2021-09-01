231928216320a99c4b3a003b536e016a2903b450
Jahneo <85087102+Jahneo@users.noreply.github.com>
2021-08-31 23:38:18 -0400

    add server

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
+const mysql = require('mysql2');
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
 
-// Default response for any other request (Not Found)
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Truedat@123',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+  });
+  // Default response for any other request (Not Found)
 app.use((req, res) => {
-  res.status(404).end();
-});
-
+    res.status(404).end();
+  });
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`);
+  });
